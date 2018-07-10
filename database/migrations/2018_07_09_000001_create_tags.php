5b1d1652acbc7763f97deecb4ce5ca8b711fd43d
masuichig <masuichig@isao.co.jp>
2018-07-10 18:24:25 +0900

    fix index

--- database/migrations/2018_07_09_000001_create_tags.php
+++ database/migrations/2018_07_09_000001_create_tags.php
@@ -14,7 +14,7 @@ class CreateTags extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->increments('id')->index();
+            $table->increments('id');
             $table->string('name');
             $table->timestamps();
         });
