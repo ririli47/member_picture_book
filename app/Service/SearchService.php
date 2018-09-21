db504d7e3cf08f0b6c32ab3d1bded4ea2fb7ddd8
ririli47 <suguha47@gmail.com>
2018-09-21 18:04:12 +0900

    ユーザー名での検索を追加

--- app/Service/SearchService.php
+++ app/Service/SearchService.php
@@ -8,12 +8,19 @@ use App\UserTag;
 
 class SearchService
 {
-    static function searchByTag(string $tagName) {
+    static function searchByTag(string $tagName)
+    {
         $tag = Tag::findByName($tagName);
 
         $user_ids = UserTag::where('tag_id', $tag['id'])->get(['user_id']);
         
         return $user_ids;
     }
-}
+    
+    static function searchByUserName(string $userName)
+    {
+        $user_ids = User::where('name', $userName)->get(['id']);
 
+        return $user_ids;
+    }
+}
